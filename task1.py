171a6ffb0298f6e051f58529b5c2a134256189ed
SimonYasn <58779921+SimonYasn@users.noreply.github.com>
2020-01-10 19:23:12 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -5,11 +5,11 @@ with open(dataset, 'r') as f:
     for i in f:
         s = i.rstrip()
         data.append(int(s))
-print("{:.2f}".format(np.percentile(data, 90)))
-print("{:.2f}".format(np.median(data)))
-print("{:.2f}".format(np.max(data)))
-print("{:.2f}".format(np.min(data)))
-print("{:.2f}".format(np.mean(data)))
+print("{:.2f}".format(np.percentile(data, 90)),'\n'
+      "{:.2f}".format(np.median(data)),'\n'
+      "{:.2f}".format(np.max(data)),'\n'
+      "{:.2f}".format(np.min(data)),'\n'
+      "{:.2f}".format(np.mean(data)))
 
 
 
