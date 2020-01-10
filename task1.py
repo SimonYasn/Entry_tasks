f2ff70d84af759ce81c390b139ea5231f33d75be
SimonYasn <58779921+SimonYasn@users.noreply.github.com>
2020-01-10 18:52:06 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,60 +1,16 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[105]:
-
-
+import numpy as np
 dataset = input()
 with open(dataset, 'r') as f:
     data = []
     for i in f:
         s = i.rstrip()
         data.append(int(s))
-
-
-# In[106]:
-
-
-import numpy as np
-
-
-# In[107]:
-
-
-data
-
-
-# In[113]:
-
-
 print("{:.2f}".format(np.percentile(data, 90)))
-
-
-# In[108]:
-
-
 print("{:.2f}".format(np.median(data)))
-
-
-# In[109]:
-
-
 print("{:.2f}".format(np.max(data)))
-
-
-# In[110]:
-
-
 print("{:.2f}".format(np.min(data)))
-
-
-# In[111]:
-
-
 print("{:.2f}".format(np.mean(data)))
 
-
-# In[ ]:
 
 
 
